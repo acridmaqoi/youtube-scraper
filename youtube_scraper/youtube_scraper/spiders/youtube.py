c59a234854e08ceffc8fd825a90ecafcc42f560a
Sam Quinn <samquinn20@gmail.com>
2021-08-08 19:42:20 +0100

    scraper very inital implementation (get video title, desc, next_url e.c.t.)

--- youtube_scraper/youtube_scraper/spiders/youtube.py
+++ youtube_scraper/youtube_scraper/spiders/youtube.py
@@ -1,10 +1,39 @@
 import scrapy
+from scrapy_splash import SplashRequest
 
 
 class YoutubeSpider(scrapy.Spider):
-    name = 'youtube'
-    allowed_domains = ['youtube.com']
-    start_urls = ['http://youtube.com/']
+    name = "youtube"
+    allowed_domains = ["youtube.com"]
+    start_urls = ["https://www.youtube.com/watch?v=Ebme2W0yF18&t=26s"]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse, args={"wait": 0.5})
 
     def parse(self, response):
-        pass
+        yield {
+            "url": response.url,
+            "embed_url": response.xpath(
+                '//*[@id="watch7-content"]/link[3]//@href'
+            ).get(),
+            "video_id": response.xpath(
+                '//*[@id="watch7-content"]/meta[5]//@content'
+            ).get(),
+            "name": response.xpath('//*[@id="watch7-content"]/meta[1]//@content').get(),
+            "description": response.xpath(
+                '//*[@id="watch7-content"]/meta[2]//@content'
+            ).get(),
+            "genre": response.xpath(
+                '//*[@id="watch7-content"]/meta[16]//@content'
+            ).get(),
+            "date_published": response.xpath(
+                '//*[@id="watch7-content"]/meta[14]//@content'
+            ).get(),
+            "genre": response.xpath(
+                '//*[@id="watch7-content"]/meta[16]//@content'
+            ).get(),
+            "next_video_url": response.xpath(
+                '//*[@id="dismissible"]/div/div[1]/a//@href'
+            ).get(),
+        }
