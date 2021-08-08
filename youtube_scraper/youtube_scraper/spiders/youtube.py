2bcc5745aeb72ee89488073360f8726c0399979a
Sam Quinn <samquinn20@gmail.com>
2021-08-08 21:18:36 +0100

    follow to next video

--- youtube_scraper/youtube_scraper/spiders/youtube.py
+++ youtube_scraper/youtube_scraper/spiders/youtube.py
@@ -5,13 +5,26 @@ from scrapy_splash import SplashRequest
 class YoutubeSpider(scrapy.Spider):
     name = "youtube"
     allowed_domains = ["youtube.com"]
-    start_urls = ["https://www.youtube.com/watch?v=Ebme2W0yF18&t=26s"]
+    start_urls = ["https://www.youtube.com/watch?v=Ebme2W0yF18"]
+
+    custom_settings = {
+        # find a way to pick another video if the first one has already been scraped
+        "DUPEFILTER_CLASS": "scrapy.dupefilters.BaseDupeFilter",
+    }
 
     def start_requests(self):
         for url in self.start_urls:
-            yield SplashRequest(url, self.parse, args={"wait": 0.5})
+            yield SplashRequest(
+                url,
+                self.parse,
+                cookies={"CONSENT": "YES+"},
+                args={"wait": 1.5},  # don't brute force wait for elm
+            )
 
     def parse(self, response):
+        # from scrapy.shell import inspect_response
+        # inspect_response(response, self)
+
         yield {
             "url": response.url,
             "embed_url": response.xpath(
@@ -37,3 +50,8 @@ class YoutubeSpider(scrapy.Spider):
                 '//*[@id="dismissible"]/div/div[1]/a//@href'
             ).get(),
         }
+
+        next_video = response.xpath('//*[@id="dismissible"]/div/div[1]/a//@href').get()
+        if next_video is not None:
+            next_video = response.urljoin(next_video)
+            yield SplashRequest(next_video, self.parse, args={"wait": 1.5})
