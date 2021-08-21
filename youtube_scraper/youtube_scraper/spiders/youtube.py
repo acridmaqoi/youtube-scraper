efd5656b5b332eeb2aeb439f16b70099fa755fbd
Sam Quinn <samquinn20@gmail.com>
2021-08-22 00:44:38 +0100

    use xpath for checking next_vid elm exists

--- youtube_scraper/youtube_scraper/spiders/youtube.py
+++ youtube_scraper/youtube_scraper/spiders/youtube.py
@@ -19,13 +19,21 @@ class YoutubeSpider(scrapy.Spider):
     function main(splash)
       splash:set_user_agent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0')
       assert(splash:go(splash.args.url))
-      
-      while not splash:select('#dismissible > div > div.metadata.style-scope.ytd-compact-video-renderer > a') do
+
+      local function isempty(s)
+        return s == nil or s == ''
+      end
+
+      local element_xpath = "//*[@id='dismissible']/div/div[1]/a//@href"
+
+
+      while isempty(splash:evaljs("document.evaluate(\\"" .. element_xpath .. "\\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue")) do
         splash:wait(0.001)
       end
 
       return {html=splash:html()}
     end
+
     """
 
     def __init__(self, start_url, **kw):
