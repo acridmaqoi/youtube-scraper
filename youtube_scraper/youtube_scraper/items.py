d1244e1850a94082bb244145ef287d7aac6726e5
Sam Quinn <samquinn20@gmail.com>
2021-08-22 20:10:00 +0100

    fix video item syntax errors

--- youtube_scraper/youtube_scraper/items.py
+++ youtube_scraper/youtube_scraper/items.py
@@ -2,12 +2,12 @@ from scrapy.item import Item, Field
 
 
 class Video(Item):
-    url: Field()
-    embed_url: Field()
-    video_id: Field()
-    name: Field()
-    description: Field()
-    thumbnail_url: Field()
-    channel: Field()
-    date_published: Field()
-    genre: Field()
+    video_id = Field()
+    url = Field()
+    embed_url = Field()
+    name = Field()
+    description = Field()
+    thumbnail_url = Field()
+    channel = Field()
+    date_published = Field()
+    genre = Field()
