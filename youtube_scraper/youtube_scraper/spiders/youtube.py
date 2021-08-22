1d5bb388eb325f38770c65af41140946070f693c
Sam Quinn <samquinn20@gmail.com>
2021-08-22 15:55:01 +0100

    move lua script into seperate file

--- youtube_scraper/youtube_scraper/spiders/youtube.py
+++ youtube_scraper/youtube_scraper/spiders/youtube.py
@@ -15,26 +15,7 @@ class YoutubeSpider(scrapy.Spider):
         "DUPEFILTER_CLASS": "scrapy.dupefilters.BaseDupeFilter",
     }
 
-    splash_script = """
-    function main(splash)
-      splash:set_user_agent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0')
-      assert(splash:go(splash.args.url))
-
-      local function isempty(s)
-        return s == nil or s == ''
-      end
-
-      local element_xpath = "//*[@id='dismissible']/div/div[1]/a//@href"
-
-
-      while isempty(splash:evaljs("document.evaluate(\\"" .. element_xpath .. "\\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue")) do
-        splash:wait(0.001)
-      end
-
-      return {html=splash:html()}
-    end
-
-    """
+    splash_script = open("youtube_scraper/spiders/splash.lua", "r").read()
 
     def __init__(self, start_url, **kw):
         super().__init__(**kw)
